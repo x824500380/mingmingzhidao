504c4b7fe261a8905eaf9141eb60deb9e8ceeb1e
wikibady <hysuperliu@hit.edu.cn>
2015-11-15 17:06:27 +0800

    search

--- zhidao/views.py
+++ zhidao/views.py
@@ -3,12 +3,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
-
+import requests
+import re
+import sys
 
 def index(request):
 	return render_to_response('index.html')
 @csrf_exempt
 def search(request):
-	return render_to_response('search.html')
+	url="http://zhidao.baidu.com/search?word="+request.POST["question"]
+	html = requests.get(url)
+	
+	
+	return render_to_response('search.html',{"html":html.text})
 def login(request):
 	return render_to_response('login.html')
