8a96907acde79f76ecdc3d8ce7d0cbd258b28d84
wikibady <hysuperliu@hit.edu.cn>
2015-11-16 13:49:25 +0800

    aa

--- zhidao/views.py
+++ zhidao/views.py
@@ -15,7 +15,9 @@ def search(request):
 	html = requests.get(url)
 	html.encoding='gbk'
 	selector = etree.HTML(html.text)
-	aim =selector.xpath('//*[@id="wgt-list"]/dl/dt/a/text()')	
-	return render_to_response('search.html',{"html":aim})
+	aaim =selector.xpath('//*[@id="wgt-list"]/dl[2]/dt/a/text()')
+	info = aaim[0].xpath('string(.)')
+	#aim = info.replace('\n'.'').replace(' '.'')	
+	return render_to_response('search.html',{"html":info})
 def login(request):
 	return render_to_response('login.html')
