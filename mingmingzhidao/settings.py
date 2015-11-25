b5964a8f5693a49a58e26a63b8db3c3402ee6664
wikibady <hysuperliu@hit.edu.cn>
2015-11-25 10:01:39 +0800

    ds

--- mingmingzhidao/settings.py
+++ mingmingzhidao/settings.py
@@ -23,7 +23,7 @@ if debug:
 
     MYSQL_DB = 'mingmingzhidao'      
     MYSQL_USER = 'root'   
-    MYSQL_PASS = '123456'   
+    MYSQL_PASS = 'root'   
     MYSQL_HOST_M = '127.0.0.1'   
     MYSQL_HOST_S = '127.0.0.1'   
     MYSQL_PORT = '3306'   
